c895c8c70d34cbba978508a664560e58933bd9cc
VHall1 <victorhallpsn@hotmail.com>
2023-11-16 17:46:48 +0000

    fix theme context type

--- app/store/theme/theme.tsx
+++ app/store/theme/theme.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import type { Theme } from "./util";
 
 interface ThemeContextType {
-  theme: Theme | null;
+  theme: Theme;
   setTheme: (theme: Theme) => void;
 }
 
